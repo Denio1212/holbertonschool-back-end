ae7c6fb5582e00a05fb89458d11b81c778e99561
Denio <shehuden22@yahoo.com>
2023-05-17 18:29:20 +0200

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,44 @@
 #!/usr/bin/python3
 """
 Retrieves information on employee To do list
+
+New stuff ***
+Get -> accesses the external source of the API to acquire information
+json -> basically used to extract the info into any program
+The idea of this is quite simple. It takes a bit to get it but
+the premise is getting the variables from the api and doing simple
+processes, But still a bit tricky to figure out since this school
+doesn't really provide a clearer explanation. They tell you to
+figure it out on your own and I always feel Braindead when i'm just
+uncomfortable with the topic since its my first try
 """
-import urllib.request
-import json
-import sys
+from requests import get
+from sys import argv
 
 
-def get_employee_todo_progress(employee_id):
-    """
-    Gets the employee to do requests
-    """
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = urllib.request.urlopen(url)
-    data = json.lo
-    total_tasks = len(data)
-    done_tasks = [task for task in data if task['completed']]
-    done_task_count = len(done_tasks)
-    employee_name = data[0]['name']
+if __name__ == "__main__":
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    todos_data = response.json()
+    completed = 0
+    total_task = 0
+    tasks = []
+    users_get = get('https://jsonplaceholder.typicode.com/users')
+    users_data = users_get.json()
 
-    print(f"Employee {employee_name} is done with tasks ({done_task_count}/{total_tasks}):")
+    for user in users_data:
+        if user.get("id") == int(argv[1]):
+            employee_name = user.get("name")
 
-    for task in done_tasks:
-        print(f"\t{task['title']}")
+    for ids in todos_data:
+        if ids.get("userId") == int(argv[1]):
+            total_task += 1
 
+            if ids.get("completed") is True:
+                completed += 1
+                tasks.append(ids.get("title"))
 
-if __name__ == '__main__':
-    """
-    doctor is the one
-    """
-    if len(sys.argv) < 2:
-        print("Please provide an employee ID as an argument.")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          completed, total_task))
+
+    for content in tasks:
+        print("\t {}".format(content))
