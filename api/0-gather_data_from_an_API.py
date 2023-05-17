bf1a76d38dcb2429e809b3f934ea155cdabd7ce6
Denio <shehuden22@yahoo.com>
2023-05-17 19:13:00 +0200

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,52 @@
 #!/usr/bin/python3
 """
-TEST inin
+Retrieves information on employee To do list
+
+New stuff ***
+Get -> accesses the external source of the API to acquire information
+json -> basically used to extract the info into any program
+text-> accesses the data in string format
+
+The idea of this is quite simple. It takes a bit to get it but
+the premise is getting the variables from the api and doing simple
+processes, But still a bit tricky to figure out since this school
+doesn't really provide a clearer explanation. They tell you to
+figure it out on your own and I always feel Braindead when i'm just
+uncomfortable with the topic since its my first try
 """
-import urllib.request
 import json
-import sys
-
-def get_employee_todo_progress(employee_id):
-    # Make a GET request to the REST API
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read().decode())
-
-    # Calculate the TODO list progress
-    total_tasks = len(data)
-    done_tasks = [task for task in data if task['completed']]
-    done_task_count = len(done_tasks)
-    employee_name = data[0]['name']
-
-    # Print the employee's TODO list progress
-    print(f"Employee {employee_name} is done with tasks ({done_task_count}/{total_tasks}):")
-
-    # Print the titles of completed tasks
-    for task in done_tasks:
-        print(f"\t{task['title']}")
+from requests import get
+from sys import argv
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please provide an employee ID as an argument.")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+if __name__ == "__main__":
+    """
+    Api script
+    """
+    emp_ID = argv[1]
+    todos = get(
+        "https://jsonplaceholder.typicode.com/todos"
+    )
+    user = get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(emp_ID)
+    )
+    data_todo = todos.text
+    data_user = user.text
+
+    current_todo = json.loads(todos.text)
+    current_user = json.loads(user.text)
+
+    task_count = 0
+    completed_todo = []
+
+    for data in current_todo:
+        if data["userID"] == current_user["id"]:
+            task_count += 1
+            if data["completed"]:
+                completed_todo.append(data)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        current_user['name'], len(completed_todo), task_count))
+
+    for done in completed_todo:
+        print(f"\t {done['title']}")
