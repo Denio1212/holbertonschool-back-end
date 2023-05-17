c59e0a037fbadf5ff1e05791b95b4bcf0d2bd90c
Denio <shehuden22@yahoo.com>
2023-05-17 19:07:36 +0200

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,6 +5,8 @@ Retrieves information on employee To do list
 New stuff ***
 Get -> accesses the external source of the API to acquire information
 json -> basically used to extract the info into any program
+text-> accesses the data in string format
+
 The idea of this is quite simple. It takes a bit to get it but
 the premise is getting the variables from the api and doing simple
 processes, But still a bit tricky to figure out since this school
@@ -14,31 +16,37 @@ uncomfortable with the topic since its my first try
 """
 from requests import get
 from sys import argv
+import json
 
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    todos_data = response.json()
-    completed = 0
-    total_task = 0
-    tasks = []
-    users_get = get('https://jsonplaceholder.typicode.com/users')
-    users_data = users_get.json()
+    """
+    Api script
+    """
+    emp_ID = argv[1]
+    todos = get(
+        "https://jsonplaceholder.typicode.com/todos"
+    )
+    user = get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(emp_ID)
+    )
+    data_todo = todos.text
+    data_user = user.text
 
-    for user in users_data:
-        if user.get("id") == int(argv[1]):
-            employee_name = user.get("name")
+    current_todo = json.loads(todos.text)
+    current_user = json.loads(user.text)
 
-    for ids in todos_data:
-        if ids.get("userId") == int(argv[1]):
-            total_task += 1
+    task_count = 0
+    completed_todo = []
 
-            if ids.get("completed") is True:
-                completed += 1
-                tasks.append(ids.get("title"))
+    for data in current_todo:
+        if data["userID"] == current_user["id"]:
+            task_count += 1
+            if data["completed"]:
+                completed_todo.append(data)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name,
-          completed, total_task))
+    print("Employee {} is done with tasks({}/{}):".format(
+        current_user['name'], len(completed_todo), task_count))
 
-    for content in tasks:
-        print("\t {}".format(content))
+    for done in completed_todo:
+        print(f"\t {done['title']}")
